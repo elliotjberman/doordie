d3274fab0b9c26d26641b0d44186896243c80c94
Tom Wall <twall@millennialmedia.com>
2016-03-22 22:41:41 -0400

    structured

---
+++ src/js/home/aboutus/index.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+export default class AboutUs extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+      console.log("HERE");
+        return (
+              <div>
+                  <div className="col-xs-12">
+                          <h1>Westgate Health Campus</h1>
+                          <h2>An Extraordinary Campus for Healthcare</h2>
+                  </div>
+
+                  <div className="col-xs-12 col-sm-8 col-md-6">
+                          <p>
+                                  Westgate Health Campus is one of the largest comprehensive healthcare developments in the Southwest.
+                                   With over 200,000 square feet of Class A office space organized in a unique five building circular configuration, it provides a thoroughly modern and integrated healthcare campus.
+                                   Based in the burgeoning City of Glendale, it lies at the heart of metropolitan Phoenix, Arizona, and at the crossroads of the main Highway 101N and I-10 corridors.
+                                   Within a short distance of the three largest health systems in Arizona, and proximity to major access including highways as well as Glendale Ave.,
+                                   it is an ideal location for a transformative healthcare campus, and an economic engine for Glendale and the West Valley.
+                                   Nearby are major entertainment and shopping venues to complement the campus which offers both condominium and rental space,
+                                   organized about complementary points of patient care, as well as related areas of research and education, and supplemental retail and pharmacy.
+                          </p>
+                  </div>
+                </div>
+                );
+    }
+}
--- src/js/home/index.jsx
+++ src/js/home/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Bootstrap from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import AboutUs from './aboutus';
 
 export default class Home extends Component {
     constructor() {
@@ -8,6 +9,7 @@ export default class Home extends Component {
     }
 
     render() {
+        console.log(AboutUs)
         return (
         	<div>
         	<nav className="navbar navbar-default" style={{backgroundColor: "#97D87F"}}>
@@ -31,22 +33,7 @@ export default class Home extends Component {
         	    </div>
         	  </div>
         	</nav>
-        				<div className="col-xs-12">
-        					<h1>Westgate Health Campus</h1>
-        					<h2>An Extraordinary Campus for Healthcare</h2>
-        				</div>
-
-        				<div className="col-xs-12 col-sm-8 col-md-6">
-        					<p>
-        						Westgate Health Campus is one of the largest comprehensive healthcare developments in the Southwest.
-        						 With over 200,000 square feet of Class A office space organized in a unique five building circular configuration, it provides a thoroughly modern and integrated healthcare campus.
-        						 Based in the burgeoning City of Glendale, it lies at the heart of metropolitan Phoenix, Arizona, and at the crossroads of the main Highway 101N and I-10 corridors.
-        						 Within a short distance of the three largest health systems in Arizona, and proximity to major access including highways as well as Glendale Ave.,
-        						 it is an ideal location for a transformative healthcare campus, and an economic engine for Glendale and the West Valley.
-        						 Nearby are major entertainment and shopping venues to complement the campus which offers both condominium and rental space,
-        						 organized about complementary points of patient care, as well as related areas of research and education, and supplemental retail and pharmacy.
-        					</p>
-        				</div>
+        		{AboutUs}
         	</div>
         	);
     }
