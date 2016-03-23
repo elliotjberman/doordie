5f83f5f81d4d8bfde197c9b46fa8adad53392a1a
Tom Wall <twall@millennialmedia.com>
2016-03-22 23:03:40 -0400

    indexes for everyone

---
+++ src/js/home/contact/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import sass from './styles.scss';
+
+export default class Contact extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+        	<div className="full-screen">
+        	</div>);
+    }
+}
