ee26a834e972b5a679e6cc5b86676d919edf703c
elliotjberman <elliotjberman@gmail.com>
2016-03-22 22:21:31 -0400

    bootstrap scrim

--- src/js/entry.js
+++
@@ -1,9 +0,0 @@
-// Import CSS
-import '../css/master.scss';
-
-// Import React and JS
-import HelloBox from './HelloBox';
-import React from 'react';
-
-// Render!
-React.render(<HelloBox />, document.getElementsByTagName('body')[0]);
