7d50f82913b3663b92466cf5b738e689567aa978
elliotjberman <elliotjberman@gmail.com>
2016-03-25 10:13:17 -0400

    mobile legibility

--- src/js/home/press/index.jsx
+++ src/js/home/press/index.jsx
@@ -13,7 +13,7 @@ export default class Press extends Component {
                   <div className="col-xs-12 col-sm-10 col-sm-offset-1 padded-container">
 										<h1 className='header'>Press</h1>
                     <div className="links">
-                      <a className="link-top" target="_blank" href="http://www.glendalestar.com/news/article_08fd792e-ea30-11e5-9fe4-7f5fccca17a1.html">The Glendale Star
+                      <a className="link link-top" target="_blank" href="http://www.glendalestar.com/news/article_08fd792e-ea30-11e5-9fe4-7f5fccca17a1.html">The Glendale Star
                         <span><br/></span>
                         <span className="title">Developer launches $30M health care campus that will bring 1,500 jobs to Glendale</span>
                         <span><br/></span>
